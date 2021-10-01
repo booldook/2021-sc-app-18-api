60ec29f3428faf7e33d6f51e22e8caba0dcce5c0
booldook <booldook@gmail.com>
2021-10-01 10:19:36 +0900

    완료

--- back/routes/api2/api-router.js
+++ back/routes/api2/api-router.js
@@ -5,17 +5,19 @@ const { pool } = require('../../modules/mysql-init')
 
 router.get('/', async (req, res, next) => {
 	let sql, token
+	console.log(req.headers)
 	try {
 		if(req.cookies.token) {
 			const verifyToken = jwt.verify(req.cookies.token, process.env.JWT_SALT)
-			token = jwt.sign({ data: verifyToken.data }, process.env.JWT_SALT, { expiresIn: '10s' })
+			token = jwt.sign({ data: verifyToken.data }, process.env.JWT_SALT, { expiresIn: '2h' })
 		}
 		else {
-			sql = " SELECT * FROM users_api WHERE apikey=? "
-			const [rs] = await pool.execute(sql, [req.query.apikey])
-			token = jwt.sign({ data: rs[0] }, process.env.JWT_SALT, { expiresIn: '10s' })
+			sql = " SELECT * FROM users_api WHERE apikey=? AND domain=? "
+			const [rs] = await pool.execute(sql, [req.query.apikey, req.headers.origin])
+			token = jwt.sign({ data: rs[0] }, process.env.JWT_SALT, { expiresIn: '2h' })
 		}
-		res.cookie('token', token, { expires: new Date(Date.now() + 10000) }).json({ success: true, msg: '쿠키발행' })
+		res.cookie('token', token, { expires: new Date(Date.now() + 10000) })
+		res.json({ success: true, msg: '쿠키발행' })
 	}
 	catch(err) {
 		res.status(500).json(err)
